59f804a4994d12f87bca6b654eb0065c4d235f9a
tomotaka <tomo17.ts@gmail.com>
2020-09-25 12:26:51 +0900

    Add files via upload

--- 02_togikai_keep_dist.py
+++ 02_togikai_keep_dist.py
@@ -53,16 +53,24 @@ L_short = 30    #initial50
 L_mid = 60
 L_long = 150     #initial100
 C_short = 30    #initial50
-C_mid = 70
+C_mid = 60
 C_long = 170    #initial100
-R_short = 30    #initial50
+R_short = 50    #initial50
 R_mid = 60
 R_long = 140     #initial100
 ERR_DIS = 500   #20190907
 action=0
 
-keep_dist = 50
-
+target_d=35
+count =0
+st_ang=0
+old_dist=np.zeros([1,3])
+count = 0
+#Kp =100/target_d
+Kp = 2
+Ki =0.05
+Kd = 0.1
+#Kd = 100/target_d
 # magarisugiru
 #massukguni recover dekinaikoto
 
@@ -89,13 +97,53 @@ def Accel(ped):
     togikai_drive.Accel(PWM_PARAM,pwm,time,ped)
 
 def Steer(ang,direction):#1:Right,-1:left
-    togikai_drive.Accel(PWM_PARAM,pwm,time,40)
+    togikai_drive.Accel(PWM_PARAM,pwm,time,50)
     togikai_drive.Steer(PWM_PARAM,pwm,time,ang*direction)
-
-def keep_dis(RLdis):
-    dis=RLdis-keep_dis
-    steer_ang=dis/keep_dis*100
-    return steer_ang
+def Steer_k(ang):#1:-100~100
+    togikai_drive.Accel(PWM_PARAM,pwm,time,50)
+    togikai_drive.Steer(PWM_PARAM,pwm,time,ang)
+    
+def keep_dist(RLdis,Ldis,angl1):
+    
+    if RLdis>Ldis:
+        now_dist = Ldis
+    else:
+        now_dist = RLdis
+    
+    if abs(old_dist[0,0]-now_dist)>100:
+        now_dist = old_dist[0,0]
+    #Kp =120/target_d
+    angl=0
+    direc=0
+    dis2 = old_dist[0,2]
+    dis1 = old_dist[0,1]
+    dis=target_d-now_dist
+    steer_ang = angl1+(dis-dis1)*Kp + Ki*dis + Kd*((dis-dis1)-(dis1-dis2))
+    print('dis{0}, now_dis{1}'.format(dis,now_dist))
+    #steer_ang=angl1+(dis-dis1)*Kp + Ki*dis + Kd*((dis-dis1)-(dis1-dis2))
+    #steer_ang =dis/target_d*120
+    #steer_ang = 100
+    
+    #zyouken seiri
+    #if steer_ang>0:
+    #    direc=1
+    #else:
+    #    direc=-1
+    #    steer_ang = abs(steer_ang)
+    #if now_dist <target_d:
+    #    angl = abs(steer_ang)
+    #    direc=1
+    #elif now_dist>target_d:
+    #    angl = abs(steer_ang)
+    #    direc=-1
+    old_dist[0,0] = now_dist
+    old_dist[0,1] = dis
+    old_dist[0,2] = dis1
+    if steer_ang>100:
+        steer_ang=100
+    if steer_ang<-100:
+        steer_ang = -100
+    return steer_ang,direc
 
 
 #ここから走行用プログラム
@@ -111,17 +159,19 @@ try:
         R_L_dis = togikai_ultrasonic.Mesure(GPIO,time,35,37)
         #RrRHセンサ距離
         R_R_dis = togikai_ultrasonic.Mesure(GPIO,time,36,38)
-
-        if R_L_dis<50-1:
-            action = 5#dummy
-            ang = abs(keep_dis(R_L_dis))
-            Steer(ang,1)
-        elif R_L_dis>50+1:
-            action = 5#dummy
-            ang = abs(keep_dis(R_L_dis))
-            Steer(ang,-1)
-        else:
-            action = 0
+        
+        #if FRdis>C_mid:
+        if count ==0:
+            old_dist[0,0] = L_dis
+            count = count+1
+        st_ang,st_direc = keep_dist(R_L_dis,L_dis,st_ang)
+        #Steer(st_ang,st_direc)
+        Steer_k(st_ang)
+        action =5
+    
+        #elif count5 == 10:
+        #    count5 = 0
+        #   action = 0
 
         #togikai_drive.Accel(PWM_PARAM,pwm,time,40)
         #togikai_drive.Steer(PWM_PARAM,pwm,time,0)
@@ -142,15 +192,17 @@ try:
         #         else:
         #             action = 22
 
-        # if FRdis<C_mid:#まえとの距離がみじかいとき旋回しろ大
-        #     if L_dis<=L_short and R_dis<=R_short:
-        #             action=4#後退
-        #     elif L_dis<=L_short and R_dis>R_short:#左が閾値未満
-        #         action = 1#右旋回
-        #     elif L_dis>L_short and R_dis<=R_short:#右が閾値未満
-        #         action =2#左旋回
-        #     elif L_dis>L_short and R_dis>R_short:
-        #         action=0
+        #if FRdis<C_mid:#まえとの距離がみじかいとき旋回しろ大
+        #    if L_dis<=L_short and R_dis<=R_short:
+         #       action=4#後退
+          #  elif L_dis<=L_short and R_dis>R_short:#左が閾値未満
+           #     action = 1#右旋回
+            #    st_ang = abs(L_dis-L_short)/L_short*100
+            #elif L_dis>L_short and R_dis<=R_short:#右が閾値未満
+             #   action =2#左旋回
+             #   st_ang = abs(R_dis-R_short)/R_short*100
+           # elif L_dis>L_short and R_dis>R_short:
+            #    action=0
 
 
         #ver1
@@ -186,7 +238,7 @@ try:
         #elif {FRdis > CAR_MIN_R*1.1}:
         #   togikai_drive.Accel(PWM_PARAM,pwm,time,40)
         #    togikai_drive.Steer(PWM_PARAM,pwm,time,0)
-        elif time.time()-start_time < 1:
+        if time.time()-start_time < 1:
             pass
         elif FRdis <=10:
             togikai_drive.Accel(PWM_PARAM,pwm,time,-100)
@@ -203,30 +255,21 @@ try:
         if action==0:
             Accel(50)
         elif action==1:
-            Steer(100,1)
-        elif action == 11:#右旋回小
-            Steer(50,1)
-        elif action == 12:#右旋回小
-            Steer(30,1)
+            Steer(st_ang,1)
         elif action==2:
-            Steer(100,-1)
-        elif action == 21:#ひだり旋回小
-            Steer(50,-1)
-        elif action == 22:#ひだり旋回小
-            Steer(30,-1)
+            Steer(st_ang,-1)
         
         elif action==3:
             steer_angle = (L_short-L_dis)/L_short*100
             Steer(100,1)
-        elif action == 4:
-            back(100)
+        
             
         
         #距離データを配列に記録
         d = np.vstack([d,[time.time()-start_time, FRdis, R_dis, L_dis]])
         #距離を表示
-        print('Fr:{0:.1f} , FrRH:{1:.1f} , FrLH:{2:.1f},action:{3:.1f}'.format(FRdis,R_dis,L_dis,action))
-        time.sleep(0.05)
+        print('Fr:{0:.1f} , FrRH:{1:.1f} , FrLH:{2:.1f}, RrLH:{3:.1f}, action:{4:.1f}, ang:{5:.1f},ste:{6:.1f}'.format(FRdis,R_dis,L_dis,R_L_dis,action,st_ang,st_direc))
+        time.sleep(0.02)
 
 except KeyboardInterrupt:
     print('stop!')
@@ -234,4 +277,3 @@ except KeyboardInterrupt:
     togikai_drive.Accel(PWM_PARAM,pwm,time,0)
     togikai_drive.Steer(PWM_PARAM,pwm,time,0)
     GPIO.cleanup()
-
